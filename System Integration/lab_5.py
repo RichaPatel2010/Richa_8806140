99475ee8f2dc6c3e8b294ee0ca487367f38f4f18
RichaPatel2010 <patelricha727@gmail.com>
2023-07-11 22:27:18 -0400

    Files added

--- System Integration/lab_5.py
+++ System Integration/lab_5.py
@@ -28,7 +28,7 @@ time.sleep(5)
 assert "laptop" in driver.title
 
 # Selecting a laptop from the search results
-laptop_link = driver.find_element("xpath","/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/span/a/div/img")
+laptop_link = driver.find_element("xpath","/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div/div/div/div/div[1]/span/a/div/img")
 # laptop_link = driver.find_element("By.CSS_SELECTOR","span[data-component-type='s-product-image'] a")
 laptop_link.click()
 
@@ -48,14 +48,15 @@ no_thanks_button= driver.find_element("xpath","/html/body/div[9]/div[3]/div[1]/d
 no_thanks_button.click()
 time.sleep(2)
 
-proceed_to_checkout= driver.find_element("xpath","/html/body/div[9]/div[3]/div[3]/div/div[1]/div[3]/div[1]/div[2]/div[3]/span/span/input")
+proceed_to_checkout= driver.find_element("xpath","/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[3]/div/div[1]/form/span/span/span/input")
 proceed_to_checkout.click()
 time.sleep(2)
 
+
 # Verifying that the laptop has been added to the cart
-# cart_count = driver.find_element("id","nav-cart-count")
-# assert cart_count.text == "1"
-# cart_count.click()
+#cart_count = driver.find_element("id","nav-cart-count")
+#assert cart_count.text == "1"
+#cart_count.click()
 
 # Closing the webdriver
 driver.close()
